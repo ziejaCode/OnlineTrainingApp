b34e8f8b1042e474b5577ff95b5cd948e40088f8
Marty <Marty@DESKTOP-N1M9EJ9>
2019-03-29 17:04:30 +0000

    test2

--- OnlineTrainingApp/src/main/java/application/Application.java
+++ OnlineTrainingApp/src/main/java/application/Application.java
@@ -12,5 +12,7 @@ public class Application {
         SpringApplication.run(Application.class, args);
         
         System.out.println("\nThis is test\n");
+        
+        System.out.println("\nThis is test no 2 \n");
     }
 }
