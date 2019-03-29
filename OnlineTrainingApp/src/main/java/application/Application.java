60e9ac1df004b77cd91e84b7ffe9ddeb7c59d249
ziejaCode <czarny25101@gmail.com>
2019-03-29 16:51:12 +0000

    test1

--- OnlineTrainingApp/src/main/java/application/Application.java
+++ OnlineTrainingApp/src/main/java/application/Application.java
@@ -10,5 +10,7 @@ public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+        
+        System.out.println("\nThis is test\n");
     }
 }
